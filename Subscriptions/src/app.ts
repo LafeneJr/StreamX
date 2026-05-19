e5e76270bc116ffaf5cafa6414684495012b2952
lafenejunior <lafene070@gmail.com>
2026-05-19 09:25:22 +0100

    second commit

--- Subscriptions/src/app.ts
+++ Subscriptions/src/app.ts
@@ -1,7 +1,6 @@
 import express from "express"
 import cors from "cors"
 import helmet from "helmet"
-
 import authRoutes from "./modules/auth/auth.route.js"
 import userRoutes from "./routes/user.routes.js"
 import planRoutes from "./routes/plan.routes.js"
@@ -12,21 +11,21 @@ import historyRoutes from "./routes/history.routes.js"
 
 const app = express()
 
+app.use(helmet())
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL,
+    origin: "http://localhost:3000",
     credentials: true,
   })
 )
 
-app.use(helmet())
-
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-app.get("/health", (_req, res) => {
-  res.status(200).json({ success: true, message: "API is running" })
+app.get("/health", (req, res) => {
+    res.status(200).json({ success: true, message: "API is running"})
 })
+
 
 app.use("/api/auth", authRoutes)
 app.use("/api/users", userRoutes)
@@ -36,14 +35,5 @@ app.use("/api/subscriptions", subRoutes)
 app.use("/api/devices", deviceRouter)
 app.use("/api/history", historyRoutes)
 app.use("/api", contentRoutes)
-
-// 404 handler
-app.use((req, res) => {
-  res.status(404).json({
-    success: false,
-    message: "Route not found",
-  })
-})
-
 
 export default app
